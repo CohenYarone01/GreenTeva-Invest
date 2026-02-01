e1182515a51f89d3dc376ff10203860a8b9581c5
CohenYarone01 <49273855+CohenYarone01@users.noreply.github.com>
2026-02-01 18:01:15 +0100

    Fix: Hard reset dependencies and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // On force Rollup à accepter le module même s'il a un doute
-      external: [], 
-    },
-  },
   optimizeDeps: {
-    include: ['@formspree/react'],
+    include: ['@formspree/react']
   },
+  build: {
+    commonjsOptions: {
+      include: [/@formspree\/react/, /node_modules/]
+    }
+  }
 })
